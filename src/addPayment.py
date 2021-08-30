41c53bd7d2e50feb0f9a69f1f38806ac351bba4b
Eden Rose Risma <65489091+edenroseFR@users.noreply.github.com>
2021-08-30 11:20:42 +0800

    Refactored addPayment

--- src/addPayment.py
+++ src/addPayment.py
@@ -8,7 +8,7 @@ class AddPayment(QtWidgets.QMainWindow):
     def __init__(self,  parent=None, creditorID=None):
         super(AddPayment,self).__init__(parent)
         loadUi('../UI File/add_payment.ui', self)
-        self.p          = parent
+        self.p = parent
         self.creditorID = creditorID
         self.configureWidgets()
 
@@ -18,14 +18,14 @@ class AddPayment(QtWidgets.QMainWindow):
 
     def pushButtonClicked(self):
         self.attendeeID = database.attendeeID(self.attendee.currentText())
-        self._amount    = int(self.amount.cleanText())
-        self._date      = self.date.date().toString("yyyy-MM-dd")
+        self._amount = int(self.amount.cleanText())
+        self._date = self.date.date().toString("yyyy-MM-dd")
 
         if self._amount != 0:
-            database.recordPayment(amount    =self._amount,
-                                   date      =self._date,
-                                   creditorID=self.creditorID,
-                                   attendeeID=self.attendeeID)
+            database.recordPayment(amount = self._amount,
+                                   date = self._date,
+                                   creditorID = self.creditorID,
+                                   attendeeID = self.attendeeID)
 
         else:
             messagebox.emptyAmount()
