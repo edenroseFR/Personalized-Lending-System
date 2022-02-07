43290c57c00b142ec224aa4bdcff70b465b5e8e7
edenroseFR <edenroserisma13@gmail.com>
2022-02-08 03:06:23 +0800

    Modify readme image

--- src/config.py
+++ src/config.py
@@ -1,15 +1,11 @@
 import mysql.connector as mysql
-from dotenv import load_dotenv
-import os
-
-load_dotenv('.env')
 
 
 database = mysql.connect(
-    host= os.getenv('DB_HOST'),
-    user=os.getenv('DB_USER'),
-    password=os.getenv('DB_PASSWORD'),
-    database=os.getenv('DB_NAME')
+    host='localhost',
+    user='your_database_username',
+    password='your_database_password',
+    database='your_database_name'
 )
 
 cursor = database.cursor()
